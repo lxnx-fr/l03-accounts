777dca7bfe4742bf276d63fcb18e94bcfbb59fb3
Laurin Noel Frank <laurinfrank03@gmail.com>
2023-01-02 01:24:07 +0100

    Fixed minimal issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
         head: {
             htmlAttrs: {
                 lang: 'en'
-
             },
             meta: [
                 { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=5.0' },
@@ -21,7 +20,5 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/robots',
         '@nuxtjs/tailwindcss',
-        'nuxt-icon',
-        '@vueuse/motion/nuxt'
     ],
 })
